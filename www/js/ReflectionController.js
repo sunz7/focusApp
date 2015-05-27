e53c501a84af1c5457481f00ad38cb217ca65506
Zhi Sun <sunz7@uw.edu>
2015-05-27 12:01:02 -0700

    uploading images

--- www/js/ReflectionController.js
+++ www/js/ReflectionController.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers')
 
 .controller('ReflectionCtrl', function($scope) {
   $scope.reflection = {
-  	image: 'https://raw.githubusercontent.com/sunz7/focusApp/master/www/img/Reflection1.png',
+  	image: '',
   	question: '',
   	myAnswser: ''
 
