a516aaa0a25493e7503207501c2d46375d0037cd
Zhi Sun <sunz7@uw.edu>
2015-05-27 12:50:11 -0700

    made changes to reflection page, changed app name

--- www/js/ChallengeController.js
+++ www/js/ChallengeController.js
@@ -1,13 +1,30 @@
 angular.module('starter.controllers')
 
-.controller('ChallengeCtrl', function($scope, Challenges) {
+.controller('ChallengeCtrl', function($scope, Challenges, $ionicPopup) {
   $scope.settings = {
     enableFriends: false
   };
+  var today = false;
   $scope.chas = Challenges.getAll();
-  $scope.addOne = function(cha){
-  	cha.completed += 1;
-  	Challenges.update(cha);
+
+    $scope.addOne = function(cha){
+    if(today){
+     var confirmPopup = $ionicPopup.confirm({
+       title: 'Try Tomorrow',
+       template: 'You already completed this challenge today'
+    });
+    confirmPopup.then(function(res) {
+     if(res) {
+        $state.go("tab.challenges");
+     }
+   });
+
+    }else{
+      today = true;
+      cha.completed += 1;
+      Challenges.update(cha);
+    }
+
   };
   $scope.remove = function(cha){
   	$scope.chas.splice($scope.chas.indexOf(cha), 1);
